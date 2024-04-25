2949979e534962cb63bc4aa471bce6006ee7bddb
RezoanulHasan <rezoanulhasan96@gmail.com>
2024-04-26 00:09:31 +0600

    update auth route

--- src/app/modules/Auth/authRoutes.ts
+++ src/app/modules/Auth/authRoutes.ts
@@ -1,8 +1,8 @@
-import express from 'express';
 import { USER_ROLE } from '../user/user.constant';
 import auth from '../../middlewares/authMiddleware';
 import validateRequest from '../../middlewares/validateRequest';
 import { AuthValidation } from './auth.validation';
+import express, { NextFunction, Request, Response } from 'express';
 import {
   changePassword,
   login,
@@ -10,11 +10,18 @@ import {
   refreshToken,
   register,
 } from './authController';
+import { upload } from '../../../utils/sendImageToCloudinary';
 
 const router = express.Router();
 
 router.post(
   '/register',
+  upload.single('file'),
+  (req: Request, res: Response, next: NextFunction) => {
+    req.body = JSON.parse(req.body.data);
+    next();
+  },
+
   validateRequest(AuthValidation.RegisterZodSchema),
   register,
 );
